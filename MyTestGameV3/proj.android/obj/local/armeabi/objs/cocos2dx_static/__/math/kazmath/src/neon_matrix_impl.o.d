b58a28fa16e13d8f6664e3b7cd602c72e39537aa
tavandung12 <itprono3@gmail.com>
2014-12-16 09:06:15 +0700

    remove files

--- MyTestGameV3/proj.android/obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/src/neon_matrix_impl.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cocos2dx_static/__/math/kazmath/src/neon_matrix_impl.o: \
- /Volumes/Working/Workspace/Cocos2dx/MyTestGameV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/src/neon_matrix_impl.c \
- /Volumes/Working/Workspace/Cocos2dx/MyTestGameV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/include/kazmath/neon_matrix_impl.h
-
-/Volumes/Working/Workspace/Cocos2dx/MyTestGameV3/proj.android/../cocos2d/cocos/2d/../math/kazmath/include/kazmath/neon_matrix_impl.h:
--- MyTestProject/cocos2d/plugin/protocols/proj.android/gen/org/cocos2dx/libAnalyticsCommon/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.cocos2dx.libAnalyticsCommon;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
